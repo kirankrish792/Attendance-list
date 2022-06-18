bb722cac6fa05cc96d55f9c1cbcbb6f4ee9df10a
Kiran KS <kirankrish792@gmail.com>
2022-06-18 18:37:05 +0530

    intial commit

---
+++ Data.js
@@ -0,0 +1,30 @@
+const { v4: uuidv4 } = require('uuid');
+
+module.exports = [
+    {
+        id: uuidv4(),
+        name: "kiran",
+        class: "Btech CS",
+    },
+    {
+        id: uuidv4(),
+        name: "kiran-2",
+        class: "Btech ECE",
+    },
+    {
+        id: uuidv4(),
+        name: "kiran-3",
+        class: "Btech EEE",
+    },
+    {
+        id: uuidv4(),
+        name: "kiran-4",
+        class: "Btech CE",
+    },
+    {
+        id: "55aa39cc-9026-428b-9c9f-8d391b76e875",
+        name: "kiran-5",
+        class: "Btech IT",
+    },
+
+]
---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const path = require('path')
+const engine = require('ejs-mate')
+const app = express()
+const methodOverride = require('method-override')
+const { v4: uuidv4 } = require('uuid');
+
+const data = require('./data')
+
+app.engine('ejs', engine)
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+app.get('/students', (req, res) => {
+    res.render('index', { data })
+})
+
+app.get("/newstudent", (req, res) => {
+    res.render("newstudent")
+})
+
+app.get('/students/:id', (req, res) => {
+    const id = req.params.id
+    const student = data.find(student => student.id === id)
+    res.render('student', { student })
+})
+
+
+app.post("/newstudent", (req, res) => {
+    req.body.id = uuidv4()
+    data.push(req.body)
+    res.redirect("/students")
+})
+
+app.listen(3000, () => {
+    console.log("Connected to port 3000")
+})
