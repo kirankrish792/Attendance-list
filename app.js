7189f11c6eb2d1437bf79b9ed85f19f42e39c58f
Bhagyarajj <107787062+Bhagyarajj@users.noreply.github.com>
2022-06-29 22:28:56 +0530

    Added css

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const engine = require('ejs-mate')
 const app = express()
 const methodOverride = require('method-override')
 const { v4: uuidv4 } = require('uuid');
-const {student}= require("./firebase-data")
+const { student } = require("./firebase-data")
 
 
 app.engine('ejs', engine)
@@ -17,19 +17,20 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.render('home')
 })
 
 
 app.get('/students', async (req, res) => {
     const fireData = await student.get();
-    let data=[]
+    let data = []
     fireData.forEach((doc) => {
         let value = doc.data();
         data.push(value)
-      });
+    });
     res.render('index', { data })
 })
 
@@ -37,40 +38,40 @@ app.get("/newstudent", (req, res) => {
     res.render("newstudent")
 })
 
-app.get('/students/:id', async(req, res) => {
+app.get('/students/:id', async (req, res) => {
     const id = req.params.id
     const fireData = await student.get();
-    let data=[]
+    let data = []
     fireData.forEach((doc) => {
         let value = doc.data();
-        if(value.id === id){
-        data.push(value)
-    }
-      });
-      const studentData = data[0]
+        if (value.id === id) {
+            data.push(value)
+        }
+    });
+    const studentData = data[0]
     res.render('student', { studentData })
 })
 
 
-app.post("/newstudent",async (req, res) => {
+app.post("/newstudent", async (req, res) => {
     let data = req.body
     data.id = uuidv4()
     const push = await student.doc(req.body.name).set(req.body)
     res.redirect('/students')
 })
 
-app.delete('/students/:id',async (req,res)=>{
+app.delete('/students/:id', async (req, res) => {
     const id = req.params.id
     const fireData = await student.get();
-    let data=[]
+    let data = []
     fireData.forEach((doc) => {
         let value = doc.data();
-        if(value.id === id){
-        data.push(doc.id)
-    }
-      });
-      const deleteData = await student.doc(data[0]).delete();
-      res.redirect('/students')
+        if (value.id === id) {
+            data.push(doc.id)
+        }
+    });
+    const deleteData = await student.doc(data[0]).delete();
+    res.redirect('/students')
 })
 
 const port = process.env.PORT || 3000
