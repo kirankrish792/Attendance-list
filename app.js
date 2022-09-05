a1ebb166553a2badd65b5896e5270e5e5774b022
Kiran KS <Kirankrish792@gmail.com>
2022-09-05 23:26:47 +0530

    enrollment completed

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const app = express()
 const methodOverride = require('method-override')
 const { v4: uuidv4 } = require('uuid');
 const { student } = require("./firebase-data")
-
+const axios = require('axios').default
+const strongPaswordGenerator = require("strong-password-generator");
 
 app.engine('ejs', engine)
 app.set('view engine', 'ejs')
@@ -56,7 +57,8 @@ app.get('/students/:id', async (req, res) => {
 app.post("/newstudent", async (req, res) => {
     let data = req.body
     data.id = uuidv4()
-    const push = await student.doc(req.body.name).set(req.body)
+    // res.send(data)
+    const push = await student.doc(data.id).set(req.body)
     res.redirect('/students')
 })
 
@@ -64,14 +66,24 @@ app.delete('/students/:id', async (req, res) => {
     const id = req.params.id
     const fireData = await student.get();
     let data = []
+    let Finger_id = ""
     fireData.forEach((doc) => {
         let value = doc.data();
         if (value.id === id) {
             data.push(doc.id)
+            Finger_id = value.Finger_id
         }
     });
+    console.log(Finger_id)
+
     const deleteData = await student.doc(data[0]).delete();
+    await axios.post("http://192.168.225.59/delete", Finger_id);   
     res.redirect('/students')
+})
+
+app.get("/pass",(req,res)=>{
+    let password = strongPaswordGenerator.generatePassword();
+    res.send(password)
 })
 
 const port = process.env.PORT || 3000
